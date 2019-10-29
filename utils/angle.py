7bbcb6e24350d3686721952c7b006211d627408e
Sean Seung Won Lee <swdev1202@gmail.com>
2019-10-28 21:14:38 -0700

    uploading last changes

--- utils/angle.py
+++ utils/angle.py
@@ -21,14 +21,14 @@ def calculate_angle(joint_record):
     f1_v1 = np.array([lsho[0] - neck[0], lsho[1] - neck[1]])
     f1_v2 = np.array([neck[0] - rsho[0], neck[1] - rsho[1]])
     
-    feature1 = find_angle(f1_v1, f1_v2)
+    feature1 = np.rad2deg(find_angle(f1_v1, f1_v2))
 
     f2_v1 = np.array([neck[0] - rsho[0], neck[1] - rsho[1]])
     f2_v2 = np.array([rsho[0] - relb[0], rsho[1] - relb[1]])
-    feature2 = find_angle(f2_v1, f2_v2)
+    feature2 = np.rad2deg(find_angle(f2_v1, f2_v2))
 
     f3_v1 = np.array([rsho[0] - relb[0], rsho[1] - relb[1]])
     f3_v2 = np.array([relb[0] - rwri[0], relb[1] - rwri[1]])
-    feature3 = find_angle(f3_v1, f3_v2)
+    feature3 = np.rad2deg(find_angle(f3_v1, f3_v2))
 
     return (feature1, feature2, feature3)
